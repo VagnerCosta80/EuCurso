af1a9f30e1c6e14a77135d811b0bf7df2d0fc6b9
VagnerCosta80 <105681476+VagnerCosta80@users.noreply.github.com>
2023-10-03 00:20:08 -0300

    Add files via upload
    Estou apenas fazendo testes

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[]args) {
+
+       //Exercio 1
+
+//       int numero = 10;
+//
+//       if(numero > 0){
+//           System.out.println("O número é positivo");
+//       }else if (numero < 0){
+//           System.out.println("O número é negativo");
+//       }else{
+//           System.out.println("O número é neutro");
+//       }
+
+        //Exercio 5
+
+        for(int i = 1; i < 11; i++){
+            System.out.println(" 7X" + i + "=" + 7 * i);
+        }
+   }
+}
