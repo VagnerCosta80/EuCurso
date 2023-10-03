a39e9c8ba980dfb7b6904975522040fd9b658b64
VagnerCosta80 <105681476+VagnerCosta80@users.noreply.github.com>
2023-10-03 00:23:27 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,6 @@
+import java.util.Scanner
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
-       //Exercio 1
-
-//       int numero = 10;
-//
-//       if(numero > 0){
-//           System.out.println("O número é positivo");
-//       }else if (numero < 0){
-//           System.out.println("O número é negativo");
-//       }else{
-//           System.out.println("O número é neutro");
-//       }
-
-        //Exercio 5
-
-        for(int i = 1; i < 11; i++){
-            System.out.println(" 7X" + i + "=" + 7 * i);
-        }
-   }
+    }
 }
